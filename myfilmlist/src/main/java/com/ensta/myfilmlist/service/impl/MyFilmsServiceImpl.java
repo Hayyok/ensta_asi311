11e360e97b3d173598b3196b3cfdc55942eb6ddd
JulesPeyr <jules.peyrache@gmail.com>
2024-12-09 10:13:14 +0100

    Utilisation d'API Stream dans l'implémentation des services

--- myfilmlist/src/main/java/com/ensta/myfilmlist/service/impl/MyFilmsServiceImpl.java
+++ myfilmlist/src/main/java/com/ensta/myfilmlist/service/impl/MyFilmsServiceImpl.java
@@ -4,7 +4,10 @@ import com.ensta.myfilmlist.model.Film;
 import com.ensta.myfilmlist.model.Realisateur;
 import com.ensta.myfilmlist.service.MyFilmsService;
 import com.ensta.myfilmlist.service.ServiceException;
-
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.Arrays;
 import java.util.List;
 
 public class MyFilmsServiceImpl implements MyFilmsService {
@@ -20,39 +23,29 @@ public class MyFilmsServiceImpl implements MyFilmsService {
             throw new ServiceException("La liste des films réalisés ne peut pas être null.");
         }
 
-        // Mise à jour du statut celebre
         boolean estCelebre = realisateur.getFilmRealises().size() >= NB_FILMS_MIN_REALISATEUR_CELEBRE;
         realisateur.setCelebre(estCelebre);
 
         return realisateur;
     }
-    public long calculerDureeTotale(List<Film> films) throws ServiceException{
+
+    @Override
+    public long calculerDureeTotale(List<Film> films) throws ServiceException {
         if (films == null) {
             throw new ServiceException("La liste de film ne peut pas être null.");
         }
-        int dureeTotale = 0;
-        for (Film film : films) {
-            if (film.getDuree() > 0) {
-                dureeTotale += film.getDuree();
-            }
-            else {
-                throw new ServiceException("Un film est null");
-            }
-        }
-        return dureeTotale;
+
+        return films.stream()
+                .mapToInt(Film::getDuree)
+                .sum();
     }
+    @Override
     public double calculerNoteMoyenne(double[] notes) {
         if (notes == null || notes.length == 0) {
             return 0;
         }
 
-        double somme = 0;
-        for (double note : notes) {
-            somme += note;
-        }
-
-        double moyenne = somme / notes.length;
-        return Math.round(moyenne * 100.0) / 100.0; // Arrondi à 2 chiffres après la virgule
+        OptionalDouble moyenneOptional = Arrays.stream(notes).average();
+        return moyenneOptional.isPresent() ? Math.round(moyenneOptional.getAsDouble() * 100.0) / 100.0 : 0;
     }
-
 }
